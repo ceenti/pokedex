fa6fc5db19d37ff4735b2f7c203119ac954d7335
Cinthia Sánchez <ceenti@gmail.com>
2022-07-10 22:51:06 -0500

    [PKM-001] feat: adjust styles

--- src/components/FrontCard/index.js
+++ src/components/FrontCard/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { string } from 'prop-types';
 import TypePill from '../TypePill';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
 const FrontCard = (props) => {
     const { title, description, image } = props;
     return (
         <div className='frontCard'>
-            <h3>#001 Bulbasaur</h3>
+            <div className='headerCard'>
+                <h3>#001 Bulbasaur</h3>
+                <FontAwesomeIcon icon={faStar}/>
+            </div>
+
             <p className='sprite'>
                 <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg' alt='Bulbasaur sprite' />
             </p>
