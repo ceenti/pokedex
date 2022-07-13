41cb565a75726c39ce43b5384e72e7ddd999d610
Cinthia Sánchez <ceenti@gmail.com>
2022-07-13 02:12:50 -0500

    [PKM-0001] feat: add query

--- src/ApolloClient/src/queries.js
+++ src/ApolloClient/src/queries.js
@@ -35,6 +35,41 @@ export const GET_ALL_POKEMON = gql`{
   }
 }`;
 
+export const GET_POKEMON = gql`query Pokemon($id: Int!) {
+  pokemon: pokemon_v2_pokemon(where: {id: {_eq: $id}}) {
+    id
+    name
+    weight
+    height
+    pokemon_v2_pokemontypes {
+      pokemon_v2_type {
+        name
+        id
+      }
+    }
+    pokemon_v2_pokemonabilities {
+      pokemon_v2_ability {
+        name
+        id
+      }
+    }
+    pokemon_v2_pokemonstats {
+        pokemon_v2_stat {
+        name
+      }
+      base_stat
+    }
+    pokemon_v2_pokemonspecy {
+      pokemon_v2_evolutionchain {
+        pokemon_v2_pokemonspecies(order_by: {id: asc}) {
+          id
+          name
+        }
+      }
+    }
+  }
+}`;
+
 export const GET_TYPES = gql`{
   types: pokemon_v2_type(where: {generation_id: {_in: [1,2]}}) {
     id
